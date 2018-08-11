c258e7d32a03edcac4717d081ed2bc1f3948eb74
µobi <minitobi.dev@gmail.com>
2018-08-11 15:45:33 +0200

    Add the keyblock class, init the grid in GameScreen Beware: blocks are NOT rendered yet!

--- core/src/com/thetriumvirate/game/GameScreen.java
+++ core/src/com/thetriumvirate/game/GameScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 
 public final class GameScreen implements Screen {
 	
@@ -13,9 +14,15 @@ public final class GameScreen implements Screen {
 	
 	// Resource paths
 	// private static final String RES_SOMETHING = "somewhere/something";
+	private static final String KEYBLOCK_TEXTURE = "assets/graphics/keyblock.png";
 	
 	private Main game;
 	private OrthographicCamera cam;
+	
+	
+	private int pixelGridWidth = 40, pixelGridHeight = 30;
+	private Keyblock[][] blocks = new Keyblock[pixelGridWidth][];
+	
 	
 	public GameScreen(Main game) {
 		this.game = game;
@@ -24,7 +31,36 @@ public final class GameScreen implements Screen {
 		cam.update();
 		game.spritebatch.setProjectionMatrix(cam.combined);
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		
+		setUpPixelGrid();
 	}
+	
+	private void setUpPixelGrid() {
+		//full init of the whole array, every entry is null so far
+		for(int i = 0; i < blocks.length; i++) {
+			blocks[i] = new Keyblock[pixelGridHeight];
+		}
+		
+		//init the levels blocks here
+		
+		String levelGen = "22,16".trim();
+				
+		//translate the String to the level
+		
+		String[] levelGenCommands = levelGen.split(";");
+		
+		for(int i = 0; i < levelGenCommands.length; i++) {
+			String[] c = levelGenCommands[i].split(",");
+			int px = Integer.parseInt(c[0]);
+			int py = Integer.parseInt(c[1]);
+			
+			//TODO implement relative edgeLength
+			blocks[px][py] = new Keyblock(px, py, 20, this);
+		}
+				
+	}
+	
+	
 
 	@Override
 	public void show() {
@@ -35,9 +71,12 @@ public final class GameScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
+	
+	
+	
 
 	public static void prefetch(AssetManager m) {
-		
+		m.load(KEYBLOCK_TEXTURE, Texture.class);
 	}
 	
 	@Override
@@ -58,6 +97,10 @@ public final class GameScreen implements Screen {
 	@Override
 	public void hide() {
 		
+	}
+	
+	public Main getGame() {
+		return this.game;
 	}
 
 	// TODO Unload all non-global resources
--- core/src/com/thetriumvirate/game/Keyblock.java
+++ core/src/com/thetriumvirate/game/Keyblock.java
@@ -1,5 +1,62 @@
 package com.thetriumvirate.game;
 
+
+
 public class Keyblock {
 
+	private int edgeLength = 20;
+	private int posX = 0, posY = 0;
+	private String letter = "ER";
+	private String ascii = "qwertzuiopasdfghjklyxcvbnm1234567890";
+	
+	
+	//attention! receives the pos in the array, not the pos in the game world!
+	public Keyblock(int arrayPosX, int arrayPosY, int edgeLength, GameScreen gs) {
+		this.edgeLength = edgeLength;
+		this.posX = arrayPosX * edgeLength;
+		this.posY = arrayPosY * edgeLength;
+		
+		//gen random char
+		letter = String.valueOf(ascii.charAt(gs.getGame().RAND.nextInt(ascii.length())));
+	}
+
+
+	public int getEdgeLength() {
+		return edgeLength;
+	}
+
+
+	public void setEdgeLength(int edgeLength) {
+		this.edgeLength = edgeLength;
+	}
+
+
+	public int getPosX() {
+		return posX;
+	}
+
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+
+	public int getPosY() {
+		return posY;
+	}
+
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+
+	public String getLetter() {
+		return letter;
+	}
+
+
+	public void setLetter(String letter) {
+		this.letter = letter;
+	}
 }

8575e4b1633c81be840bc5663e1e27e64f27fde5
Maxi <maxi.poellinger@gmail.com>
2018-08-13 21:39:17 +0200

    a

--- core/src/com/thetriumvirate/game/Player.java
+++ core/src/com/thetriumvirate/game/Player.java
@@ -12,10 +12,10 @@ public class Player {
 	private static final String RES_PLAYER_TEXTURE_DOWN = "graphics/player_texture_down.png";
 	// TODO Fix values
 	private static final float GRAVITATIONAL_ACCELERATION = Main.DEBUG ? -50 : -800;
-	private static final float MOVEMENT_ACCELERATION = Main.DEBUG ? 50 : 200;
+	private static final float MOVEMENT_ACCELERATION = Main.DEBUG ? 50 : 800;
 	private static final float JUMP_SPEED = Main.DEBUG ? 100 : 300;
 	private static final float DOUBLE_JUMP_SPEED = Main.DEBUG ? 100 : 230;
-	private static final float AIR_RESISTANCE = Main.DEBUG ? 0.5f : 0.6f;
+	private static final float AIR_RESISTANCE = Main.DEBUG ? 0.5f : -0.3f;
 	private static final int DEFAULT_WIDTH = 30;
 	private static final int DEFAULT_HEIGHT = 60;
 	
@@ -72,7 +72,7 @@ public class Player {
 		}else if(movingleft) {
 			speed.x -= MOVEMENT_ACCELERATION * delta;
 		}
-		speed.x = speed.x * (1f - ((1f - AIR_RESISTANCE) * delta));
+		speed.x = speed.x * (1f - ((1f - AIR_RESISTANCE) * delta * 3.5f));
 		// Gravity
 		speed.y += GRAVITATIONAL_ACCELERATION * delta;
 		speed.y = speed.y * (1f - ((1f - AIR_RESISTANCE) * delta));
